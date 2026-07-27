47755b9a1b7132ad30240c6ae356295cb22745bd
ivansimango02-debug <ivansimango02@gmail.com>
2026-07-27 15:44:45 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="saisip-v1";const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
-self.addEventListener("push",e=>{let d={title:"SaiSip 💧",body:"Saiiiii 😭💧 DRINK."};try{d={...d,...e.data.json()}}catch{}e.waitUntil(self.registration.showNotification(d.title,{body:d.body,icon:"./icon.svg",badge:"./icon.svg"}))});
